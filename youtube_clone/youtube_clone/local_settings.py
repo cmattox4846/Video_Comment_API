077a39a5a2fc309a95e2ad98c09a6563cb2362e5
KasebrotII <kasebrot@live.com>
2021-11-09 11:18:07 -0600

    installed django and connected to mysql

---
+++ youtube_clone/youtube_clone/local_settings.py
@@ -0,0 +1,20 @@
+
+
+
+SECRET_KEY = 'django-insecure-0yr9r$uijinq)0jt9-!s4o!)ft1+n3=x^lf^=#+kj^02q3qg(6'
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'youtube_clone',
+        'USER': 'root',
+        'PASSWORD': 'THEpasswordis82$$',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
+    }
+}

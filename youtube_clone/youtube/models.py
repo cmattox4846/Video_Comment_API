54d59920e60710428fa74b0ec31fcea73a7cdc5a
cmattox4846 <cmattox4846@gmail.com>
2021-11-09 16:57:03 -0500

    added views and urls

--- youtube_clone/youtube/models.py
+++ youtube_clone/youtube/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Comments(models.Model):
     video_ID = models.CharField(max_length=50)
-    comment = models.CharField(max_length=200)
-    comment_reply= models.CharField(max_length=200)
-    likes = models.IntegerField()
-    dislikes = models.IntegerField()
+    comment = models.CharField(max_length=200, null=True,blank=True)
+    comment_reply= models.CharField(max_length=200,null=True,blank=True)
+    likes = models.IntegerField(null=True,blank=True)
+    dislikes = models.IntegerField(null=True,blank=True)
 
--- youtube_clone/youtube/views.py
+++ youtube_clone/youtube/views.py
@@ -2,7 +2,7 @@
 from django.db.models import fields
 from django.http.response import Http404
 from django.shortcuts import render
-from .models import Video
+from .models import Comments, Video
 from .serializers import CommentSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,9 +10,9 @@ from rest_framework import  status
 
 
 # Create your views here.
-class VideoList(APIView):
+class CommentList(APIView):
     def get(self,request):
-        Video = Video.objects.all()
+        Video = Comments.objects.all()
         serializer = CommentSerializer(Video, many=True)
         return Response(serializer.data)
 
@@ -23,43 +23,43 @@ class VideoList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class VideoDetails(APIView):
+class CommentDetails(APIView):
     def get_object(self,pk):
         try:
-            return Video.objects.get(pk=pk)
-        except Video.DoesNotExist:
+            return Comments.objects.get(pk=pk)
+        except Comments.DoesNotExist:
             raise Http404
    
     def get(self,request, pk ):
-        Video = self.get_object(pk)
-        serializer = CommentSerializer(Video)
+        Comment = self.get_object(pk)
+        serializer = CommentSerializer(Comment)
         return Response(serializer.data)
 
     def put(self,request,pk):
-        Video = self.get_object(pk)
-        serializer = CommentSerializer(Video, data=request.data)
+        Comment = self.get_object(pk)
+        serializer = CommentSerializer(Comment, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self,request,pk):
-        Video = self.get_object(pk)
-        Video.delete()
+        Comment = self.get_object(pk)
+        Comment.delete()
         return Response(status=status.HTTP_200_OK)
 
     
     
-class VideoLike(APIView):
+class CommentLike(APIView):
     def get_object(self,pk):
         try:
-            return Video.objects.get(pk=pk)
-        except Video.DoesNotExist:
+            return Comments.objects.get(pk=pk)
+        except Comments.DoesNotExist:
             raise Http404
 
     def put(self,request,pk):
-        Video = self.get_object(pk)
-        serializer = CommentSerializer.increase_like(Video, Video)
+        Comment= self.get_object(pk)
+        serializer = CommentSerializer.increase_like(Comment, Comment)
         
         
        
@@ -68,13 +68,13 @@ class VideoLike(APIView):
 class VideoDislike(APIView):
     def get_object(self,pk):
         try:
-            return Video.objects.get(pk=pk)
-        except Video.DoesNotExist:
+            return Comments.objects.get(pk=pk)
+        except Comments.DoesNotExist:
             raise Http404
 
     def put(self,request,pk):
-        Video = self.get_object(pk)
-        serializer = CommentSerializer.increase_dislike(Video, Video)
+        Comment = self.get_object(pk)
+        serializer = CommentSerializer.increase_dislike(Comment, Comment)
         
         
        
